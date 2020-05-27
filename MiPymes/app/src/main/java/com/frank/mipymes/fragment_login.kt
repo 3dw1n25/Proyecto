fb9be1fe75a00ecb6576e9ca6f7ee4731e6b20d0
3dw1n25 <3dw1n2000@gmail.com>
2020-05-27 15:17:08 -0600

    haciendo cambios a fragmentos

--- MiPymes/app/src/main/java/com/frank/mipymes/fragment_login.kt
+++ MiPymes/app/src/main/java/com/frank/mipymes/fragment_login.kt
@@ -5,19 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.lastchance.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class fragment_agenda : Fragment() {
+class fragment_login : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agenda, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
 }
