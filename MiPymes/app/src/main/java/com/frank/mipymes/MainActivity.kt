7dee6c57c6bda5d51137a20a8599535aa68bc6a8
FranciscoMorazan1823 <52218500+FranciscoMorazan1823@users.noreply.github.com>
2020-05-28 20:27:13 -0600

    agregando botton funcion

--- MiPymes/app/src/main/java/com/frank/mipymes/MainActivity.kt
+++ MiPymes/app/src/main/java/com/frank/mipymes/MainActivity.kt
@@ -3,6 +3,13 @@ package com.frank.mipymes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lastchance.R
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,4 +17,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    val button = view.findViewById<Button>(R.id.navigate_destination_button)
+    button?.setOnClickListener {
+        findNavController().navigate(R.id.flow_step_one_dest, null)
+    }
+
+    val options = navOptions {
+        anim {
+            enter = R.anim.slide_in_right
+            exit = R.anim.slide_out_left
+            popEnter = R.anim.slide_in_left
+            popExit = R.anim.slide_out_right
+
+        } }
+
+
+    view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
+        findNavController().navigate(R.id.flow_step_one_dest, null, options)
+    }
+
+    view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
+    Navigation.createNavigateOnClickListener(R.id.next_action, null)
+    )
+
+
+
 }
